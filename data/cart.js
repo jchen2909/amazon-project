21ffa76ac39a0e46356b96795ac1619f0054add5
jackychen2909 <jackychen2909@gmail.com>
2024-06-25 17:30:07 -0400

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,5 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'))||
+[
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
@@ -7,6 +8,11 @@ export let cart = [
     quantity: 1
   }
 ];
+
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
   let matchingItem;
@@ -26,6 +32,8 @@ export function addToCart(productId){
       quantity: 1
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -37,4 +45,5 @@ export function removeFromCart(productId){
   })
 
   cart = newCart;
+  saveToStorage();
 }
