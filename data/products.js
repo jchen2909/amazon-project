9be825d95d5c310214a13a27a5fb3b23a81aa1aa
jackychen2909 <jackychen2909@gmail.com>
2024-07-24 16:35:02 -0400

    Used backend to load products, and updated the tests so that they work with  backend.

--- data/products.js
+++ data/products.js
@@ -85,6 +85,7 @@ const object3 = {
 object3.method();
 */
 
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -750,4 +751,24 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
+
+export let products = [];
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', ()=>{
+    products = JSON.parse(xhr.response).map((productDetails)=>{
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('Load Products');
+    fun();
+  })
+  xhr.open('GET', 'http://supersimplebackend.dev/products')
+  xhr.send();
+}
+
 
--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,11 +1,18 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage, cart, removeFromCart} from '../../data/cart.js';
+import {loadProducts} from '../../data/products.js';
 
 
 describe('Test Suide: renderOrderSummary', ()=>{
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
-  
+  //By giving the parameter 'done' the function will move on once you call the done() function. This allows us to wait for certain responses to return before moving on. (Asychronous Functions)
+  beforeAll((done)=>{
+    loadProducts(()=>{
+      done();
+    });
+  });
+
   beforeEach(()=>{
     spyOn(localStorage, 'setItem');
 
