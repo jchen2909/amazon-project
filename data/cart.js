94f296dfafa7fcd3b5860e585891103847c30b36
jackychen2909 <jackychen2909@gmail.com>
2024-07-24 16:59:07 -0400

    Learned  promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -94,3 +94,13 @@ export function updateDeliveryOption(productId, deliveryOptionId){
 
   saveToStorage();
 }
+
+export function loadCart(fun){
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', ()=>{
+    console.log(xhr.response);
+    fun();
+  })
+  xhr.open('GET', 'http://supersimplebackend.dev/cart')
+  xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,11 +2,59 @@ import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts} from '../data/products.js'
+import {loadCart} from '../data/cart.js';
+
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
-loadProducts(()=>{
+
+//Practicing Promises (a class) Instantiation requires a function
+// then() takes a function that will be run next
+
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{ 
+      resolve('value1');
+    })
+  }),
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  })
+]).then(()=>{
   renderOrderSummary();
   renderPaymentSummary();
   renderCheckoutHeader();
 });
+
+/*
+new Promise((resolve)=>{
+  loadProducts(()=>{ //LoadProducts must take in resolve() or else resolve will run before loadProduct finishes.
+    resolve('value1');
+  });
+}).then((value)=>{
+  console.log(value);
+  return new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  });
+}).then(()=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+});
+*/
+
+
+/*
+loadProducts(()=>{
+  loadCart(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+  })
+});
+*/
+
 
