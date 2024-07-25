6d674309e2639565182e52de5197c30f9e1d0423
jackychen2909 <jackychen2909@gmail.com>
2024-07-25 16:12:18 -0400

    Create order using the backend

---
+++ data/orders.js
@@ -0,0 +1,11 @@
+export const orders = JSON.parse(localStorage.getItem('orders')) || [];
+
+export function addOrder(order){
+  orders.unshift(order); //Adds the order to the front of the array
+  saveToStorage();
+}
+
+function saveToStorage(){
+  localStorage.setItem('orders', JSON.stringify(orders));
+}
+
